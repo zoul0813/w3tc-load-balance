890e8ae5feed8f6c72b1e0c82ac52dfcdb24636a
David Higgins <dhiggins@aimmedia.com>
2013-07-29 13:37:07 -0400

    added support for cloud front - pulls admin media off of S3 backend

--- w3tc-load-balance.php
+++ w3tc-load-balance.php
@@ -103,7 +103,15 @@ class W3TC_LoadBalance {
 		
 		$site_url = parse_url(get_bloginfo('url'));
 		$this->debug("get_base_url -> " . print_r($site_url, true));
-		return 'http://' . $this->w3tc_options['cdn.s3.bucket'] . '.s3.amazonaws.com' . $site_url['path'] . '/wp-content/uploads';
+		
+		switch($this->w3tc_options['cdn.engine']) {
+			case 's3': {
+				return 'http://' . $this->w3tc_options['cdn.s3.bucket'] . '.s3.amazonaws.com' . $site_url['path'] . '/wp-content/uploads';				
+			} break;
+			case 'cf': {
+				return 'http://' . $this->w3tc_options['cdn.cf.bucket'] . '.s3.amazonaws.com' . $site_url['path'] . '/wp-content/uploads';				
+			}
+		}
 	}
 	
 	function upload_dir($data) {
